8ea24984ff19de72caa0e3f0d63cae0604041f67
Aidan Benton <aidabent117@ed.amdsb.ca>
2023-02-16 10:03:47 -0500

    hjh

--- SimonSays/GameScreen.Designer.cs
+++ SimonSays/GameScreen.Designer.cs
@@ -80,8 +80,8 @@
             // 
             // GameScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
             this.Controls.Add(this.yellowButton);
             this.Controls.Add(this.blueButton);
